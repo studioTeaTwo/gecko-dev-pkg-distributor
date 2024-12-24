1dde81ed68be42a6d34ba7d1613c9dda05216511
T2 <studioteatwo@proton.me>
2024-12-24 14:16:11 +0000

    Merge branch 'fix/v0.1.0' into 'mvp'
    Fix/v0.1.0
    
    See merge request studioteatwo/gecko-dev-for-ssi!13

--- toolkit/components/ssi/protocols/Bitcoin.sys.mjs
+++ toolkit/components/ssi/protocols/Bitcoin.sys.mjs
@@ -3,17 +3,19 @@ import { bytesToHex } from "resource://gre/modules/shared/utils-hashes.sys.mjs";
 
 export const Bitcoin = {
   BIP340: {
-    sign: async(message, guid) => {
+    sign: async (message, guid) => {
       // Get private key from store
-      const credentials = await Services.ssi.searchCredentialsAsync({guid});
-      if (credentials.length === 0 ) return ""
+      const credentials = await Services.ssi.searchCredentialsAsync({ guid });
+      if (credentials.length === 0) {
+        return "";
+      }
       const key = credentials[0].secret;
 
       // Sign
       const signature = schnorr.sign(message, key);
-      const hexSignature = bytesToHex(signature)
+      const hexSignature = bytesToHex(signature);
 
       return hexSignature;
-    }
-  }
-}
+    },
+  },
+};
