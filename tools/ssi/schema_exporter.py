53b8ff4eceb8ac4b5410dee626c6954c4a83058d
T2 <studioteatwo@proton.me>
2025-01-22 19:27:13 +0900

    feat(doc): add xpidl in API docs

--- tools/ssi/schema_exporter.py
+++ tools/ssi/schema_exporter.py
@@ -2,10 +2,12 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# This script formats and exports builtin-api schema files by markdown for the docs site.
+# This script formats and exports schema files by markdown for the docs site.
 #
-# The design is based on MDN: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/action
-# schema spec: https://firefox-source-docs.mozilla.org/toolkit/components/extensions/webextensions/schema.html
+# webextensions spec: https://firefox-source-docs.mozilla.org/toolkit/components/extensions/webextensions/schema.html
+# xpidl spec: https://firefox-source-docs.mozilla.org/xpcom/xpidl.html
+#
+# The markdown design is based on MDN: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/action
 
 import json
 import os
@@ -22,45 +24,89 @@ repository_name = 'gecko-dev-for-ssi'
 repository_url = f"https://gitlab.com/studioteatwo/{repository_name}/-/blob/mvp/"
 doc_list = [
     'browser/components/extensions/schemas/ssi/ssi.json',
-    'browser/components/extensions/schemas/ssi/ssi.nostr.json'
+    'browser/components/extensions/schemas/ssi/ssi.nostr.json',
+    'toolkit/components/ssi/nsICredentialInfo.idl',
+    'toolkit/components/ssi/nsICredentialMetaInfo.idl'
 ]
 file_paths = []
 for doc in doc_list:
     file_paths.append(os.path.join(topsrcdir, doc))
 
 # output
-output_directory = "output"
-if os.path.exists(output_directory):
-    shutil.rmtree(output_directory)
-os.makedirs(output_directory)
+output_directory = os.path.join(topsrcdir, "tools/ssi/output")
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+idl_directory = os.path.join(output_directory, "core")
+if os.path.exists(idl_directory):
+    shutil.rmtree(idl_directory)
+os.makedirs(idl_directory)
 
 
-def main():
-    for file_path in file_paths:
-        file_name = os.path.basename(file_path).replace(".json", "")
-        os.makedirs(os.path.join(output_directory, file_name))
-        output_file_path = os.path.join(here, output_directory, file_name, f"README.md")
+def xpidl(file_path):
+    file_name = os.path.basename(file_path).replace(".idl", "")
+    output_file_path = os.path.join(topsrcdir, idl_directory, f"{file_name}.md")
 
-        print(f"-----{file_name}-----")
+    print(f"-----{file_name}-----")
 
-        try:
-            with open(file_path, 'r', encoding='utf-8') as json_file:
-                data = json.load(json_file)
+    top_line = [f"# {file_name}\n\n"] + [f"This is described in [XPIDL](https://firefox-source-docs.mozilla.org/xpcom/xpidl.html) that is an Interface Description Language used to specify XPCOM interface classes.\n\n"]
 
-            # Returns the text of the summary and create each member file nested inside it.
-            summary_text = build(data)
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
 
-            with open(output_file_path, 'w', encoding='utf-8') as output_file:
-                output_file.write(summary_text)
+        # assume license
+        lines = lines[4:]
 
-            print(f"-----end:{file_name}-----")
+        delete_conditions = [
+            "#include",
+            "#define",
+            "[scriptable",
+            "%",
+        ]
+        filtered_lines = [
+            line for line in lines
+            if not any(line.startswith(condition) for condition in delete_conditions)
+        ]
+        filtered_lines = remove_empty_lines(filtered_lines)
 
-        except FileNotFoundError:
-            print(f"Error: The file {file_path} was not found.")
-        except json.JSONDecodeError:
-            print(f"Error: Invalid JSON format in {file_path}.")
-        except Exception as e:
-            print(f"Error: An unexpected error occurred while processing {file_path} - {e}")
+        print(f"filtered_lines: {len(filtered_lines)}")
+
+        output_lines = top_line + ['```c++\n'] + filtered_lines + ['```\n\n'] + [get_repository_text(file_name)]
+
+        with open(output_file_path, 'w', encoding='utf-8') as output_file:
+            output_file.writelines(output_lines)
+
+        print(f"-----end:{file_name}-----")
+
+    except FileNotFoundError:
+        print(f"Error: The file {file_path} was not found.")
+    except Exception as e:
+        print(f"Error: An unexpected error occurred while processing {file_path} - {e}")
+
+
+def webextensions(file_name, file_path):
+    output_file_path = os.path.join(here, output_directory, file_name, f"README.md")
+
+    print(f"-----{file_name}-----")
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as json_file:
+            data = json.load(json_file)
+
+        # Returns the text of the summary and create each member file nested inside it.
+        summary_text = build(data)
+
+        with open(output_file_path, 'w', encoding='utf-8') as output_file:
+            output_file.write(summary_text)
+
+        print(f"-----end:{file_name}-----")
+
+    except FileNotFoundError:
+        print(f"Error: The file {file_path} was not found.")
+    except json.JSONDecodeError:
+        print(f"Error: Invalid JSON format in {file_path}.")
+    except Exception as e:
+        print(f"Error: An unexpected error occurred while processing {file_path} - {e}")
 
 
 def build(data):
@@ -113,10 +159,10 @@ def create_member_file(namespace, type, data):
     if type in ['functions', 'events'] :
         output_text += '## Syntax\n\n'
         output_text += f"{build_syntax(namespace, type, data)}\n\n"
-
-    if 'properties' in data:
-        output_text += f"### {'Type' if type == 'types' else 'Properties'}\n\n"
-        output_text += f"{build_properties(data['properties'])}"
+    elif type == 'types':
+        output_text += f"## Type\n\n"
+        output_text += f"{build_types(data['properties'])}"
+    # type == 'Properties' is not implemented.
 
     if 'parameters' in data:
         if type == 'events':
@@ -165,13 +211,13 @@ def build_parameters(data):
 
     return output_text
 
-def build_properties(data):
+def build_types(data):
     output_text = ''
     for key in data:
-        print(f"[build_properties]: {key} proceeds...")
+        print(f"[build_types]: {key} proceeds...")
 
         value = data[key]
-        output_text += f"#### `{key}`"
+        output_text += f"### `{key}`"
         if 'optional' in value and value['optional']:
             output_text += ' (optional)'
         output_text += '\n\n'
@@ -228,9 +274,40 @@ def get_repository_text(namespace):
     repository = repository_url
     if namespace == 'ssi':
         repository += doc_list[0]
+        ext = 'json'
     elif namespace == "ssi.nostr":
         repository += doc_list[1]
+        ext = 'json'
+    elif namespace == "nsICredentialInfo":
+        repository += doc_list[2]
+        ext = 'idl'
+    elif namespace == "nsICredentialMetaInfo":
+        repository += doc_list[3]
+        ext = 'idl'
 
-    return f"```admonish\nThis documentation is derived from [{namespace}.json]({repository}) in {repository_name}.\n```\n\n"
+    return f"```admonish\nThis documentation is derived from [{namespace}.{ext}]({repository}) in {repository_name}.\n```\n\n"
+
+def remove_empty_lines(lines):
+    start = 0
+    while start < len(lines) and not lines[start].strip():
+        start += 1
+
+    end = len(lines) - 1
+    while end >= start and not lines[end].strip():
+        end -= 1
+
+    return lines[start:end + 1]
+
+def main():
+    for file_path in file_paths:
+        if '.json' in os.path.basename(file_path):
+            file_name = os.path.basename(file_path).replace(".json", "")
+            sub_directory = os.path.join(output_directory, file_name)
+            if os.path.exists(sub_directory):
+                shutil.rmtree(sub_directory)
+            os.makedirs(sub_directory)
+            webextensions(file_name, file_path)
+        else:
+            xpidl(file_path)
 
 main()
