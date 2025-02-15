1c36700b0a3a7adc804fc2bab15a10001fc57d14
T2 <studioteatwo@proton.me>
2025-02-15 16:30:15 +0900

    chore(doc): add warning of typescript only

--- tools/ssi/schema_exporter.py
+++ tools/ssi/schema_exporter.py
@@ -363,6 +363,9 @@ def create_member_file_window(namespace, kind, data):
         output_text += '()'
     output_text += '\n\n'
 
+    if kind == 2097152:
+        output_text += f"```admonish attention\nThis is the difinition on TypeScript, does not exist in the execution environment. It's just a documentation commentary.\n```\n\n"
+
     if 'comment' in data:
         for node in data['comment']['summary']:
             output_text += node["text"]
